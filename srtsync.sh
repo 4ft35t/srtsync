b259eb2c936ed817ef7ee0ee55571d5c6fc5dcc5
git <git@127.1>
2022-06-05 18:50:08 +0800

    convert chn srt to utf-8 encoding

--- srtsync.sh
+++ srtsync.sh
@@ -13,6 +13,12 @@ sub_eng_index=$(ffmpeg -i "$video" 2>&1 | fgrep Subtitle | fgrep eng | head -1 |
 sub_eng_index=$((sub_eng_index-2))
 ffmpeg -y -i "$video" -map 0:s:$sub_eng_index $sub_in_video
 
+# 中文字幕转 utf-8 编码
+input_encode=$(file --mime-encoding "$srt" | awk '{print $NF}')
+input_encode=${input_encode@U}
+output_encode=UTF-8
+iconv --from-code=$input_encode --to-code=$output_encode "$srt" > "$srt".utf8 && mv "$srt".utf8 $srt
+
 srtsync.py $sub_in_video "$srt"
 rm $sub_in_video
 echo 'Done'
